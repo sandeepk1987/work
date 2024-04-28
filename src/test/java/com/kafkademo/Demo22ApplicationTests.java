46b3604126e3747749b10242291557dbcc2fe24f
Sandeep Kulkarni <sandeep1987kulkarni@gmail.com>
2024-04-28 13:30:07 +0530

    initial commit

---
+++ src/test/java/com/kafkademo/Demo22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
